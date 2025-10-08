248a50da61a53dfac4a10f4ab7b985b3a5c80652
Nikhil Patel <nnnp80@gmail.com>
2025-10-08 19:07:28 +0530

    browser: update to handle different cases

--- browser.py
+++ browser.py
@@ -23,114 +23,102 @@ class USwitchPrices:
         driver = webdriver.Firefox(service=Service('/Users/nikhil/_Drive_E/python/selenium/geckodriver'), options=options)
         driver.maximize_window()
 
-        driver.get('https://www.uswitch.com/gas-electricity/journey/postcode')
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.NAME, 'postcode'))
-        )
+        try:
+            driver.get('https://www.uswitch.com/gas-electricity/journey/postcode')
+            WebDriverWait(driver, 200).until(
+                EC.presence_of_element_located((By.NAME, 'postcode'))
+            )
 
-        accept_cookies = driver.find_element(By.CLASS_NAME, 'ucb-btn-accept--desktop')
-        accept_cookies.click()
-        sleep(2)
-
-        pin_code_box = driver.find_element(By.NAME, 'postcode')
-        pin_code_box.clear()
-        pin_code_box.send_keys(self.post_code)
-        pin_code_box.send_keys(Keys.ENTER)
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.NAME, 'addresses'))
-        )
-
-        address_box = Select(driver.find_element(By.NAME, 'addresses'))
-        address_selected = address_box.select_by_index(1)
-
-        confirm_ownership = driver.find_element(By.CLASS_NAME, '_checkboxText_18pt4_7')
-        confirm_ownership.click()
-
-        def continue_aka_submit(wait: bool = True):
-            continue_1 = driver.find_element(By.XPATH, "//button[@type='submit']")
-            continue_1.click()
-            sleep(5)
-
-        def skip():
-            skip_1 = driver.find_element(By.CLASS_NAME, '_skipButton_1p1eo_29')
-            skip_1.click()
+            accept_cookies = driver.find_element(By.CLASS_NAME, 'ucb-btn-accept--desktop')
+            accept_cookies.click()
             sleep(2)
 
-        print('Clicking Address confirmation')
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        )
+            pin_code_box = driver.find_element(By.NAME, 'postcode')
+            pin_code_box.clear()
+            pin_code_box.send_keys(self.post_code)
+            pin_code_box.send_keys(Keys.ENTER)
+            try:
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.NAME, 'addresses'))
+                )
+            except Exception:
+                return 'failed'
 
-        print('Clicking Elecricity Payment Type')
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        )
+            address_box = Select(driver.find_element(By.NAME, 'addresses'))
+            address_selected = address_box.select_by_index(1)
 
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        )
+            confirm_ownership = driver.find_element(By.CLASS_NAME, '_checkboxText_18pt4_7')
+            confirm_ownership.click()
 
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        )
+            def continue_aka_submit(wait: bool = True):
+                continue_1 = driver.find_element(By.XPATH, "//button[@type='submit']")
+                continue_1.click()
+                sleep(5)
 
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        )
+                if 'Looks like you have a business meter' in driver.page_source:
+                    return 'failed'
 
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        )
+                if wait is True:
+                    WebDriverWait(driver, 200).until(
+                        EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
+                    )
 
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@type='submit']"))
-        )
+            while True:
+                status = continue_aka_submit(wait=True)
+                if 'email-address-input' in driver.page_source:
+                    break
 
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.ID, 'email-address-input'))
-        )
+                if status == 'failed':
+                    return 'failed'
 
-        f = faker.Faker()
-        fake_email = f.email().split('@')[0] + '@gmail.com'
+            f = faker.Faker()
+            fake_email = f.email().split('@')[0] + '@gmail.com'
 
-        email = driver.find_element(By.ID, 'email-address-input')
-        email.send_keys(fake_email)
-        sleep(5)
+            email = driver.find_element(By.ID, 'email-address-input')
+            email.send_keys(fake_email)
+            sleep(5)
 
-        continue_aka_submit()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.CLASS_NAME, '_filterButton_1un9x_1'))
-        )
+            continue_aka_submit(wait=False)
+            sleep(5)
 
-        filter_ = driver.find_element(By.CLASS_NAME, '_filterButton_1un9x_1')
-        filter_.click()
-        sleep(2)
+            if len(driver.find_elements(By.CLASS_NAME, '_smartMeterInterestContent_mkazr_1')) > 0:
+                smart_meter_interest = driver.find_elements(By.NAME, 'smart-meter-interest')
+                smart_meter_interest[1].click()
+                continue_aka_submit(wait=False)
 
-        show_all_plan_button = driver.find_elements(By.NAME, 'filters.onlyShowFulfillable')
-        show_all_plan_button[1].click()
-        sleep(1)
+            WebDriverWait(driver, 200).until(
+                EC.presence_of_element_located((By.CLASS_NAME, '_filterButton_1un9x_1'))
+            )
 
-        show_results = driver.find_element(By.XPATH, "//button[@type='submit']")
-        show_results.click()
-        WebDriverWait(driver, 200).until(
-            EC.presence_of_element_located((By.CLASS_NAME, '_filterButton_1un9x_1'))
-        )
+            filter_ = driver.find_element(By.CLASS_NAME, '_filterButton_1un9x_1')
+            filter_.click()
+            sleep(2)
 
-        self.cookies = driver.get_cookies()
-        driver.quit()
+            show_all_plan_button = driver.find_elements(By.NAME, 'filters.onlyShowFulfillable')
+            show_all_plan_button[1].click()
+            sleep(1)
+
+            show_results = driver.find_element(By.XPATH, "//button[@type='submit']")
+            show_results.click()
+            WebDriverWait(driver, 200).until(
+                EC.presence_of_element_located((By.CLASS_NAME, '_filterButton_1un9x_1'))
+            )
+
+            self.cookies = driver.get_cookies()
+
+            return 'success'
+        
+        except Exception:
+            return 'failed'
+        
+        finally:
+            driver.quit()
 
     def main(self):
-        self.browser_session()
-        return self.cookies
-    
+        status = self.browser_session()
+        if status == 'success':
+            return self.cookies
+
 
 if __name__ == '__main__':
     uswitch = USwitchPrices('WS13 8PE')
